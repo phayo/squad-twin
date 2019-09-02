014b5667f053e616ffd888f98f57166840178872
phayo <mrphayo@MrPhayo-PC.localdomain>
2019-09-02 12:54:32 +0100

    initial commit

---
+++ application.py
@@ -0,0 +1,6 @@
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
