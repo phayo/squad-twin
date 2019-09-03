86bce1c151381c18025cdad23fe712890184e4f6
Victor Bassey <viktorbasi545@gmail.com>
2019-09-03 11:46:25 +0100

    Define register route

--- application.py
+++ application.py
@@ -43,6 +43,10 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
 
 
 
